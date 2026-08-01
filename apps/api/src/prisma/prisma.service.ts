38bfe55d49e9f26fcb61617b05b549230c292e92
zoaavela <enzooabdi@gmail.com>
2026-08-01 23:18:40 +0200

    feat: connect PrismaService to NestJS (CJS client + pg adapter)

---
+++ apps/api/src/prisma/prisma.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor() {
+        super({
+            adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+        });
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
+    }
+}
